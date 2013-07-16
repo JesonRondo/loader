7f3dbeb7f744051eb73533f4cf2aef5f17d5ad33
longzhou <pancnlz@gmail.com>
2013-07-16 12:06:18 +0800

    add css script

--- js/test.js
+++ js/test.js
@@ -1,13 +1,9 @@
 var test = {
-	print: function() {
-		if (console && console.log) {
-			console.log('for test');
-		} else {
-			alert('for test');
-		}
-	}
+    print: function() {
+        $('body').append('<p>document ready call</p>');
+    }
 };
 
 $(document).ready(function() {
-	test.print();
+    test.print();
 });
